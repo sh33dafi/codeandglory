325f4402d9ed5753d9bd3ceed747cd76bf611a77
Yannick Houbrix <yannick.houbrix@gmail.com>
2016-01-08 21:32:00 +0100

    Added logging

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var static = require('node-static');
 var file = new static.Server();
+var port = process.env.PORT || 3000;
+
+console.log("Going to serve on port:", port);
 
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
   }).resume();
-}).listen(process.env.PORT || 3000);
+}).listen(port);
